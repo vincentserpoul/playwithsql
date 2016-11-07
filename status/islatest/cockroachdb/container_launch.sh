9f59db62ec1c54670e56e982a0a37170493c0ea2
vincentserpoul <vincent@serpoul.com>
2016-11-07 23:16:48 +0800

    launching the containers for testing purposes

--- status/islatest/cockroachdb/container_launch.sh
+++ status/islatest/cockroachdb/container_launch.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-docker run -dit --name roach1  -p 26257:26257 -p 8080:8080 cockroachdb/cockroach:beta-20161027  start --insecure
+docker rm -f roach1;
+docker run -dit --name roach1  -p 26257:26257 -p 8080:8080 cockroachdb/cockroach:beta-20161027  start --insecure;
+sleep 5s;
+docker exec -it roach1 ./cockroach sql --execute="CREATE DATABASE entityone_test;";
+
+# to launch the tests benchmark
+# go test -db=cockroachdb -bench=.  -test.benchtime=3s
--- status/islatest/mysql/container_launch.sh
+++ status/islatest/mysql/container_launch.sh
@@ -1,2 +1,21 @@
 #!/bin/sh
-docker run -dit --name mariadb -e MYSQL_ROOT_PASSWORD=test -p 3306:3306 mariadb
+
+initdb() {
+    sleep 15s;
+    docker exec -it mydb mysql -u root -ptest -e 'CREATE DATABASE entityone_test';
+}
+
+removeContainer() {
+    docker rm -f mydb;
+}
+echo "Choose your flavor:"
+select flavor in "mysql 8.0.0" "percona 5.7.15" "mariadb 10.1.18"; do
+    case $flavor in
+        "mysql 8.0.0" ) removeContainer;docker run -dit --name mydb -e MYSQL_ROOT_PASSWORD=test -p 3306:3306 mysql:8.0.0;initdb;break;;
+        "percona 5.7.15" ) removeContainer;docker run -dit --name mydb -e MYSQL_ROOT_PASSWORD=test -p 3306:3306 percona:5.7.15;initdb;break;;
+        "mariadb 10.1.18" ) removeContainer;docker run -dit --name mydb -e MYSQL_ROOT_PASSWORD=test -p 3306:3306 mariadb:10.1.18;initdb;break;;
+    esac
+done
+
+# to launch the tests benchmark
+# go test -db=mysql -bench=.  -test.benchtime=3s
