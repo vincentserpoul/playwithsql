40ed69846a599766a6ad2ecddf24bb1f516d571f
vincentserpoul <vincent@serpoul.com>
2016-12-20 22:54:10 +0800

    check error on remove file

--- dbhandler/sqlite.go
+++ dbhandler/sqlite.go
@@ -14,7 +14,13 @@ type SQLiteDB struct{}
 
 // NewDBHandler connect to db and return the connection
 func (sqliteConf *SQLiteDB) NewDBHandler() (*sqlx.DB, error) {
-	_ = os.Remove("./test.db")
+
+	if _, err := os.Stat("./test.db"); os.IsExist(err) {
+		errRem := os.Remove("./test.db")
+		if errRem != nil {
+			return nil, errRem
+		}
+	}
 
 	db := sqlx.MustConnect("sqlite3", "./test.db")
 
