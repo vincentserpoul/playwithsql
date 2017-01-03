547f2f0f0c976a9990d13b6aaefbe099961b5099
vincentserpoul <vincent@serpoul.com>
2017-01-03 22:01:26 +0800

    ln with the right path

--- infra/install_ubuntu.sh
+++ infra/install_ubuntu.sh
@@ -3,8 +3,6 @@
 ## sqlite3 needs
 apt-get install -y sqlite3 gcc
 
-
-
 ## ORACLE NEEDS
 apt-get install -y alien libaio1 unzip pkg-config
 mkdir -p /opt/oracle
@@ -12,20 +10,20 @@ cd /opt/oracle
 unzip /root/instantclient-basic-linux.x64-12.1.0.2.0.zip
 unzip /root/instantclient-sdk-linux.x64-12.1.0.2.0.zip
 cd /opt/oracle/instantclient_12_1
-echo "export LD_LIBRARY_PATH=/opt/oracle/instantclient_12_1:/opt/oracle/instantclient_12_1/sdk/include" >> ~/.bashrc 
-echo "export PKG_CONFIG_PATH=/opt/oracle" >> ~/.bashrc 
-echo "export PATH=/opt/oracle/instantclient_12_1:$PATH" >> ~/.bashrc 
-echo "export ORACLE_HOME=/opt/oracle/instantclient_12_1:/opt/oracle/instantclient_12_1/sdk/include" >> ~/.bashrc 
+echo "export LD_LIBRARY_PATH=/opt/oracle/instantclient_12_1:/opt/oracle/instantclient_12_1/sdk/include" >> /root/.bashrc 
+echo "export PKG_CONFIG_PATH=/opt/oracle" >> /root/.bashrc 
+echo "export PATH=/opt/oracle/instantclient_12_1:$PATH" >> /root/.bashrc 
+echo "export ORACLE_HOME=/opt/oracle/instantclient_12_1:/opt/oracle/instantclient_12_1/sdk/include" >> /root/.bashrc 
 exec bash 
-ln -s /opt/oracle/libclntsh.so.12.1 /opt/oracle/libclntsh.so
-ln -s /opt/oracle/libocci.so.12.1 /opt/oracle/libocci.so
+ln -s /opt/oracle/instantclient_12_1/libclntsh.so.12.1 /opt/oracle/instantclient_12_1/libclntsh.so
+ln -s /opt/oracle/instantclient_12_1/libocci.so.12.1 /opt/oracle/instantclient_12_1/libocci.so
 
 ## Install GO
 cd /root
 wget https://storage.googleapis.com/golang/go1.7.4.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.7.4.linux-amd64.tar.gz
-echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc 
-echo "export GOPATH=$HOME" >> ~/.bashrc 
+echo "export PATH=$PATH:/usr/local/go/bin" >> /root/.bashrc 
+echo "export GOPATH=$HOME" >> /root/.bashrc 
 exec bash
 
 ## Install playwithsql
