bba074ce6e74471c46e8bbcd557b85b1c2de5987
vincentserpoul <vincent@serpoul.com>
2017-01-12 03:11:58 +0800

    launch script improvement

--- infra/databases/docker_local/mysql/container_launch.sh
+++ infra/databases/docker_local/mysql/container_launch.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 
-initdb() {
+initdb () {
     sleep 60s;
     docker exec -i mydb mysql -u root -ptest -e 'CREATE DATABASE entityone_test';
     docker exec -i mydb mysql -u root -ptest -e 'CREATE DATABASE playwithsql';
 }
 
-removeContainer() {
+removeContainer () {
     docker rm -f mydb;
 }
 
-echo "Choose your flavor:"
-select flavor in "mysql 8.0.0" "percona 5.7.16" "mariadb 10.1.20"; do
-    case $flavor in
-        "mysql 8.0.0" ) removeContainer;docker-compose -f ./infra/databases/docker_local/mysql/docker-compose-solo-mysql.yml up -d;initdb;break;;
-        "percona 5.7.16" ) removeContainer;docker-compose -f ./infra/databases/docker_local/mysql/docker-compose-solo-percona.yml up -d;initdb;break;;
-        "mariadb 10.1.20" ) removeContainer;docker-compose -f ./infra/databases/docker_local/mysql/docker-compose-solo-maria.yml up -d;initdb;break;;
-    esac
-done
+runContainer () {
+    removeContainer;
+    docker-compose -f ./infra/databases/docker_local/mysql/docker-compose-solo-$1.yml up -d;
+    initdb;
+}
+
+if [ -z "$1" ]
+then
+    echo "Choose your flavor:"
+    select flavor in "mysql 8.0.0" "percona 5.7.16" "mariadb 10.1.20"; do
+        case $flavor in
+            "mysql 8.0.0" ) runContainer "mysql";break;;
+            "percona 5.7.16" ) runContainer "percona";break;;
+            "mariadb 10.1.20" ) runContainer "maria";break;;
+        esac
+    done
+else
+    runContainer $1
+fi
 
 # to launch the tests benchmark
 # ./mysql/container_launch.sh;go test -db=mysql -bench=.  -test.benchtime=3s;docker rm -f mydb;
--- status/launch_all_remote.sh
+++ status/launch_all_remote.sh
@@ -13,3 +13,63 @@ ssh -i $YOURPRIVATEKEY core@$COREOSIPBENCH "
     docker run -t --name pws-cmd vincentserpoul/playwithsql-cmd -db=cockroachdb -host=$COREOSIPDB1 -loops=10000 &&
     docker rm -f pws-cmd"
 ssh -i $YOURPRIVATEKEY core@$COREOSIPDB1 "docker rm -f roach1"
+
+# run mssql container
+ssh -i $YOURPRIVATEKEY core@$COREOSIPDB1 "
+    PATH='/opt/bin:/usr/bin' && cd /home/core/playwithsql &&
+    ./infra/databases/docker_local/mssql/container_launch.sh"
+ssh -i $YOURPRIVATEKEY core@$COREOSIPBENCH "
+    docker rm -f pws-cmd &&
+    docker run -t --name pws-cmd vincentserpoul/playwithsql-cmd -db=mssql -host=$COREOSIPDB1 -loops=10000 &&
+    docker rm -f pws-cmd"
+ssh -i $YOURPRIVATEKEY core@$COREOSIPDB1 "docker rm -f mssqldb"
+
+# run mysql container
+ssh -i $YOURPRIVATEKEY core@$COREOSIPDB1 "
+    PATH='/opt/bin:/usr/bin' && cd /home/core/playwithsql &&
+    ./infra/databases/docker_local/mysql/container_launch.sh mysql"
+ssh -i $YOURPRIVATEKEY core@$COREOSIPBENCH "
+    docker rm -f pws-cmd &&
+    docker run -t --name pws-cmd vincentserpoul/playwithsql-cmd -db=mysql -host=$COREOSIPDB1 -loops=10000 &&
+    docker rm -f pws-cmd"
+ssh -i $YOURPRIVATEKEY core@$COREOSIPDB1 "docker rm -f mydb"
+
+# run percona container
+ssh -i $YOURPRIVATEKEY core@$COREOSIPDB1 "
+    PATH='/opt/bin:/usr/bin' && cd /home/core/playwithsql &&
+    ./infra/databases/docker_local/mysql/container_launch.sh percona"
+ssh -i $YOURPRIVATEKEY core@$COREOSIPBENCH "
+    docker rm -f pws-cmd &&
+    docker run -t --name pws-cmd vincentserpoul/playwithsql-cmd -db=mysql -host=$COREOSIPDB1 -loops=10000 &&
+    docker rm -f pws-cmd"
+ssh -i $YOURPRIVATEKEY core@$COREOSIPDB1 "docker rm -f mydb"
+
+# run maria container
+ssh -i $YOURPRIVATEKEY core@$COREOSIPDB1 "
+    PATH='/opt/bin:/usr/bin' && cd /home/core/playwithsql &&
+    ./infra/databases/docker_local/mysql/container_launch.sh maria"
+ssh -i $YOURPRIVATEKEY core@$COREOSIPBENCH "
+    docker rm -f pws-cmd &&
+    docker run -t --name pws-cmd vincentserpoul/playwithsql-cmd -db=mysql -host=$COREOSIPDB1 -loops=10000 &&
+    docker rm -f pws-cmd"
+ssh -i $YOURPRIVATEKEY core@$COREOSIPDB1 "docker rm -f mydb"
+
+# run oracle container
+ssh -i $YOURPRIVATEKEY core@$COREOSIPDB1 "
+    PATH='/opt/bin:/usr/bin' && cd /home/core/playwithsql &&
+    ./infra/databases/docker_local/oracle/container_launch.sh"
+ssh -i $YOURPRIVATEKEY core@$COREOSIPBENCH "
+    docker rm -f pws-cmd &&
+    docker run -t --name pws-cmd vincentserpoul/playwithsql-cmd -db=oracle -host=$COREOSIPDB1 -loops=10000 &&
+    docker rm -f pws-cmd"
+ssh -i $YOURPRIVATEKEY core@$COREOSIPDB1 "docker rm -f oracledb"
+
+# run postgres container
+ssh -i $YOURPRIVATEKEY core@$COREOSIPDB1 "
+    PATH='/opt/bin:/usr/bin' && cd /home/core/playwithsql &&
+    ./infra/databases/docker_local/postgres/container_launch.sh"
+ssh -i $YOURPRIVATEKEY core@$COREOSIPBENCH "
+    docker rm -f pws-cmd &&
+    docker run -t --name pws-cmd vincentserpoul/playwithsql-cmd -db=postgres -host=$COREOSIPDB1 -loops=10000 &&
+    docker rm -f pws-cmd"
+ssh -i $YOURPRIVATEKEY core@$COREOSIPDB1 "docker rm -f postgresdb"
