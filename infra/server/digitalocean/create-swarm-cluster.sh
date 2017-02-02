f75b5a8daa9b643a961506dfab9ae3fc15826510
vincentserpoul <vincent@serpoul.com>
2017-02-02 23:15:48 +0800

    complete setup with docker-machine

--- infra/server/digitalocean/create-swarm-cluster.sh
+++ infra/server/digitalocean/create-swarm-cluster.sh
@@ -2,5 +2,34 @@
 
 DOTOKEN=$1
 DOSSHKEYPATH=$2
+DOSSHFINGERPRINT=$3
 
-docker-machine create --driver digitalocean --digitalocean-access-token=$DOTOKEN --digitalocean-region=sgp1 --digitalocean-image=coreos-stable --digitalocean-ssh-key-fingerprint=9a:03:08:b9:bc:a0:fc:6a:20:6d:86:9f:18:c0:e8:ed --digitalocean-ssh-user=core --digitalocean-ssh-key-path=$DOSSHKEYPATH --swarm node1
+#=========================
+# Creating cluster members
+#=========================
+for n in $(seq 1 3); do
+    docker-machine create --driver digitalocean --digitalocean-access-token=$DOTOKEN \
+        --digitalocean-region=sgp1 \
+        --digitalocean-image=coreos-stable \
+        --digitalocean-ssh-key-fingerprint=$DOSSHFINGERPRINT \
+        --digitalocean-ssh-user=core \
+        --digitalocean-ssh-key-path=$DOSSHKEYPATH \
+        node$n
+done;
+
+#===============
+# Starting swarm
+#===============
+MANAGER_IP=$(docker-machine ip node1)
+
+docker-machine ssh node1 docker swarm init --advertise-addr $MANAGER_IP
+
+#===============
+# Adding members
+#===============
+MANAGER_TOKEN=$(docker-machine ssh node1 docker swarm join-token --quiet manager)
+WORKER_TOKEN=$(docker-machine ssh node1 docker swarm join-token --quiet worker)
+
+for n in $(seq 2 3); do
+    docker-machine ssh node$n docker swarm join --token $MANAGER_TOKEN $MANAGER_IP:2377
+done;
