ec069fe8b6fd73d0b449abac71d465b7c00b9d03
vincentserpoul <vincent@serpoul.com>
2017-04-02 16:28:31 +0000

    finally get rid of mssql non official image

--- infra/databases/docker_swarm/mssql/launch-solo.sh
+++ infra/databases/docker_swarm/mssql/launch-solo.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 initdb () {
-    sleep 60s;
+    sleep 45s;
     CONTAINER_NAME=$(docker ps --format '{{.Names}}' | grep pws_mssql);
-    docker exec -i $CONTAINER_NAME /bin/bash -c 'echo "create database entityone_test;" > createdb.sql && /usr/bin/sqlcmd -U sa -P thank5MsSQLforcingMe -i ./createdb.sql;';
-    docker exec -i $CONTAINER_NAME /bin/bash -c 'echo "create database playwithsql;" > createdb.sql && /usr/bin/sqlcmd -U sa -P thank5MsSQLforcingMe -i ./createdb.sql;';
+    docker exec -i $CONTAINER_NAME /bin/bash -c 'echo "create database entityone_test;" > createdb.sql && /opt/mssql-tools/bin/sqlcmd -U sa -P thank5MsSQLforcingMe -i ./createdb.sql;';
+    docker exec -i $CONTAINER_NAME /bin/bash -c 'echo "create database playwithsql;" > createdb.sql && /opt/mssql-tools/bin/sqlcmd -U sa -P thank5MsSQLforcingMe -i ./createdb.sql;';
 }
 
 removeService () {
