9b9e521e5ece9b23529527bf337f1ce64b469828
vincentserpoul <vincent@serpoul.com>
2017-04-28 17:03:17 +0000

    cap 100 loops

--- bench/status/kubernetes/run-all.sh
+++ bench/status/kubernetes/run-all.sh
@@ -3,13 +3,13 @@
 ./infra/server/kubernetes/create.sh
 
 echo "[" > ./bench/status/kubernetes/results.log
-./infra/databases/kubernetes/cockroachdb/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh cockroachdb cockroachdb 1000 2ms 100;
-./infra/databases/kubernetes/mssql/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh mssql mssql 1000 2ms 100;
-./infra/databases/kubernetes/mysql/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh mysql mysql 1000 4ms 1000;
-./infra/databases/kubernetes/mariadb/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh mariadb mariadb 1000 4ms 1000;
-./infra/databases/kubernetes/percona/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh percona percona 1000 4ms 1000;
-./infra/databases/kubernetes/oracle/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh oracle oracle 1000 3ms 100;
-./infra/databases/kubernetes/postgres/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh postgres postgres 1000 2ms 100;
+./infra/databases/kubernetes/cockroachdb/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh cockroachdb cockroachdb 100 2ms 100;
+./infra/databases/kubernetes/mssql/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh mssql mssql 100 2ms 100;
+./infra/databases/kubernetes/mysql/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh mysql mysql 100 4ms 1000;
+./infra/databases/kubernetes/mariadb/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh mariadb mariadb 100 4ms 1000;
+./infra/databases/kubernetes/percona/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh percona percona 100 4ms 1000;
+./infra/databases/kubernetes/oracle/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh oracle oracle 100 3ms 100;
+./infra/databases/kubernetes/postgres/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh postgres postgres 100 2ms 100;
 ./bench/status/kubernetes/run-bench.sh sqlite pws_sqlite 100 10ms 100;
 
 #  remove last comma 
