2b9891a8450f2be90370b82fc3bfbae14cfba99a
vincentserpoul <vincent@serpoul.com>
2017-04-29 17:13:53 +0000

    bench fix

--- bench/status/swarm/run-bench.sh
+++ bench/status/swarm/run-bench.sh
@@ -15,11 +15,12 @@ docker service create \
     vincentserpoul/playwithsql-cmd-status \
     -db=$DB -host=$HOST -loops=$LOOPS -pausetime=$PAUSETIME -maxconns=$MAXCONNS 
 
+sleep 2s;
 
-while [ $(docker service ls | grep $DB  | awk '{print $4}' | grep "0/" | wc -l) -lt 1 ] ;do 
+while [ $(docker service ls | grep $DB | awk '{print $4}' | grep "0/" | wc -l) != 1 ] ;do 
     sleep 1s;
 done;
 
 docker service logs pws_cmd-$DB | awk '{ print $3."," }' >> ./bench/status/swarm/results.log;
-# docker service rm pws_cmd-$DB;
-# docker service rm pws_$DB;
+docker service rm pws_cmd-$DB;
+docker service rm pws_$DB;
