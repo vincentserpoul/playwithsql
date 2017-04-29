b6b079dafcd540c3eba2c86b1130d54090e61ce1
vincentserpoul <vincent@serpoul.com>
2017-04-29 16:30:38 +0000

    better detection of finish task

--- bench/status/swarm/run-bench.sh
+++ bench/status/swarm/run-bench.sh
@@ -6,7 +6,6 @@ LOOPS=$3
 PAUSETIME=$4
 MAXCONNS=$5
 
-
 docker service rm pws_cmd-$DB
 
 docker service create \
@@ -16,9 +15,11 @@ docker service create \
     vincentserpoul/playwithsql-cmd-status \
     -db=$DB -host=$HOST -loops=$LOOPS -pausetime=$PAUSETIME -maxconns=$MAXCONNS 
 
-WAITTILFINISH=$(($LOOPS/10));
-sleep $WAITTILFINISH;
+
+while [ $(docker service ls | grep $DB  | awk '{print $4}' | grep "0/" | wc -l) -lt 1 ] ;do 
+    sleep 1s;
+done;
 
 docker service logs pws_cmd-$DB | awk '{ print $3."," }' >> ./bench/status/swarm/results.log;
-docker service rm pws_cmd-$DB;
-docker service rm pws_$DB;
+# docker service rm pws_cmd-$DB;
+# docker service rm pws_$DB;
