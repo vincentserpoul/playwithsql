9339ab9ab991ee42fe483467aa7da454eb8a6abe
vincentserpoul <vincent@serpoul.com>
2017-04-29 18:11:05 +0000

    less loops

--- bench/status/kubernetes/run-all.sh
+++ bench/status/kubernetes/run-all.sh
@@ -3,14 +3,14 @@
 ./infra/server/kubernetes/create.sh
 
 echo "[" > ./bench/status/kubernetes/results.log
-./infra/databases/kubernetes/cockroachdb/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh cockroachdb cockroachdb 100000 2ms $(($(nproc --al) * 2 + 1));
-./infra/databases/kubernetes/mssql/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh mssql mssql 100000 2ms 32767;
-./infra/databases/kubernetes/mysql/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh mysql mysql 100000 4ms 1000;
-./infra/databases/kubernetes/mariadb/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh mariadb mariadb 100000 4ms 1000;
-./infra/databases/kubernetes/percona/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh percona percona 100000 4ms 1000;
-./infra/databases/kubernetes/oracle/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh oracle oracle 100000 3ms 1;
-./infra/databases/kubernetes/postgres/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh postgres postgres 100000 2ms 100;
-./bench/status/kubernetes/run-bench.sh sqlite pws_sqlite 100000 10ms 100;
+./infra/databases/kubernetes/cockroachdb/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh cockroachdb cockroachdb 10000 2ms $(($(nproc --al) * 2 + 1));
+./infra/databases/kubernetes/mssql/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh mssql mssql 10000 2ms 32767;
+./infra/databases/kubernetes/mysql/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh mysql mysql 10000 4ms 1000;
+./infra/databases/kubernetes/mariadb/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh mariadb mariadb 10000 4ms 1000;
+./infra/databases/kubernetes/percona/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh percona percona 10000 4ms 1000;
+./infra/databases/kubernetes/oracle/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh oracle oracle 10000 3ms 1;
+./infra/databases/kubernetes/postgres/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh postgres postgres 10000 2ms 100;
+./bench/status/kubernetes/run-bench.sh sqlite pws_sqlite 10000 10ms 100;
 
 #  remove last comma 
 sed -i '$s/,$//' ./bench/status/kubernetes/results.log;
