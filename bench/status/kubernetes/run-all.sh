f62097cd6e587297e13da6e9b550bc48b6ffdea2
vincentserpoul <vincent@serpoul.com>
2017-04-29 16:34:01 +0000

    adjust max connections

--- bench/status/kubernetes/run-all.sh
+++ bench/status/kubernetes/run-all.sh
@@ -3,14 +3,14 @@
 ./infra/server/kubernetes/create.sh
 
 echo "[" > ./bench/status/kubernetes/results.log
-./infra/databases/kubernetes/cockroachdb/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh cockroachdb cockroachdb 100 2ms 100;
-./infra/databases/kubernetes/mssql/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh mssql mssql 100 2ms 100;
-./infra/databases/kubernetes/mysql/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh mysql mysql 100 4ms 1000;
-./infra/databases/kubernetes/mariadb/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh mariadb mariadb 100 4ms 1000;
-./infra/databases/kubernetes/percona/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh percona percona 100 4ms 1000;
-./infra/databases/kubernetes/oracle/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh oracle oracle 100 3ms 100;
-./infra/databases/kubernetes/postgres/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh postgres postgres 100 2ms 100;
-./bench/status/kubernetes/run-bench.sh sqlite pws_sqlite 100 10ms 100;
+./infra/databases/kubernetes/cockroachdb/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh cockroachdb cockroachdb 100000 2ms $(($(nproc --al) * 2 + 1));
+./infra/databases/kubernetes/mssql/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh mssql mssql 100000 2ms 32767;
+./infra/databases/kubernetes/mysql/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh mysql mysql 100000 4ms 1000;
+./infra/databases/kubernetes/mariadb/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh mariadb mariadb 100000 4ms 1000;
+./infra/databases/kubernetes/percona/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh percona percona 100000 4ms 1000;
+./infra/databases/kubernetes/oracle/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh oracle oracle 100000 3ms 30;
+./infra/databases/kubernetes/postgres/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh postgres postgres 100000 2ms 100;
+./bench/status/kubernetes/run-bench.sh sqlite pws_sqlite 100000 10ms 100;
 
 #  remove last comma 
 sed -i '$s/,$//' ./bench/status/kubernetes/results.log;
--- bench/status/swarm/run-all.sh
+++ bench/status/swarm/run-all.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "[" > ./bench/status/swarm/results.log
-./infra/databases/swarm/cockroachdb/launch-solo.sh && ./bench/status/swarm/run-bench.sh cockroachdb pws_cockroachdb 100 5ms 100;
-./infra/databases/swarm/mssql/launch-solo.sh && ./bench/status/swarm/run-bench.sh mssql pws_mssql 100 5ms 3;
-./infra/databases/swarm/mysql/launch-solo.sh && ./bench/status/swarm/run-bench.sh mysql pws_mysql 100 5ms 100;
-./infra/databases/swarm/mariadb/launch-solo.sh && ./bench/status/swarm/run-bench.sh mariadb pws_mariadb 100 5ms 100;  
-./infra/databases/swarm/percona/launch-solo.sh && ./bench/status/swarm/run-bench.sh percona pws_percona 100 5ms 100;  
-./infra/databases/swarm/oracle/launch-solo.sh && ./bench/status/swarm/run-bench.sh oracle pws_oracle 100 5ms 100;  
+./infra/databases/swarm/cockroachdb/launch-solo.sh && ./bench/status/swarm/run-bench.sh cockroachdb pws_cockroachdb 100 5ms $(($(nproc --al) * 2 + 1));
+./infra/databases/swarm/mssql/launch-solo.sh && ./bench/status/swarm/run-bench.sh mssql pws_mssql 100 5ms 32767;
+./infra/databases/swarm/mysql/launch-solo.sh && ./bench/status/swarm/run-bench.sh mysql pws_mysql 100 5ms 1000;
+./infra/databases/swarm/mariadb/launch-solo.sh && ./bench/status/swarm/run-bench.sh mariadb pws_mariadb 100 5ms 1000;  
+./infra/databases/swarm/percona/launch-solo.sh && ./bench/status/swarm/run-bench.sh percona pws_percona 100 5ms 1000;  
+./infra/databases/swarm/oracle/launch-solo.sh && ./bench/status/swarm/run-bench.sh oracle pws_oracle 100 5ms 30;  
 ./infra/databases/swarm/postgres/launch-solo.sh && ./bench/status/swarm/run-bench.sh postgres pws_postgres 100 5ms 100;
 ./bench/status/swarm/run-bench.sh sqlite pws_sqlite 100 10ms 100;
 
