78d194df8a35afe286f0bc855347ccfa3f59f672
vincentserpoul <vincent@serpoul.com>
2017-05-03 15:42:18 +0800

    adjust maxconns and loops

--- bench/status/kubernetes/run-all.sh
+++ bench/status/kubernetes/run-all.sh
@@ -2,15 +2,17 @@
 
 ./infra/server/kubernetes/create.sh
 
+# Number of connections is based on n1-standard-1 (1 vCPU, 3.75 GB memory) Machine type on glcoud
+
 echo "[" > ./bench/status/kubernetes/results.log
 ./infra/databases/kubernetes/cockroachdb/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh cockroachdb cockroachdb 1000 3;
-./infra/databases/kubernetes/mssql/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh mssql mssql 100000 32767;
-./infra/databases/kubernetes/mysql/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh mysql mysql 100000 1000;
-./infra/databases/kubernetes/mariadb/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh mariadb mariadb 100000 1000;
-./infra/databases/kubernetes/percona/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh percona percona 100000 1000;
-./infra/databases/kubernetes/oracle/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh oracle oracle 100000 1;
-./infra/databases/kubernetes/postgres/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh postgres postgres 100000 100;
-./bench/status/kubernetes/run-bench.sh sqlite pws_sqlite 100000 100;
+./infra/databases/kubernetes/mssql/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh mssql mssql 1000 10;
+./infra/databases/kubernetes/mysql/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh mysql mysql 1000 100;
+./infra/databases/kubernetes/mariadb/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh mariadb mariadb 1000 100;
+./infra/databases/kubernetes/percona/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh percona percona 1000 100;
+./infra/databases/kubernetes/oracle/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh oracle oracle 1000 1;
+./infra/databases/kubernetes/postgres/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh postgres postgres 1000 100;
+./bench/status/kubernetes/run-bench.sh sqlite pws_sqlite 1000 1;
 
 #  remove last comma 
 sed -i '$s/,$//' ./bench/status/kubernetes/results.log;
