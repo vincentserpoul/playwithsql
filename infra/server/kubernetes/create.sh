6b40f0037ea4976a45456ca2221fc195e5f661be
vincentserpoul <vincent@serpoul.com>
2017-05-28 18:01:50 +0800

    cleanup, kubernetes version 1.6.4

--- infra/server/kubernetes/create.sh
+++ infra/server/kubernetes/create.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 
-# prerequisites for helm installed
-
-# make sure you have enough quotas
-# 
-# for DB in cockroachdb mssql mysql oracle postgres;
-# do
-#     gcloud container clusters create $DB \
-#         --zone=asia-southeast1-a --num-nodes=4 --preemptible \
-#         --cluster-version=1.6.1 --async
-# done;
-
-# helm install --name cockroachdb stable/cockroachdb
-# gcloud container clusters get-credentials cockroachdb --zone asia-southeast1-a --project playwithsql
-# gcloud container clusters delete cockroachdb -q
-
-
 gcloud container clusters create benchcluster \
     --zone=us-central1-f --num-nodes=4 --machine-type=n1-standard-1 \
     --cluster-version=1.6.4;
 
 gcloud container clusters get-credentials benchcluster --zone us-central1-f --project playwithsql;
+
+# gcloud container clusters delete benchcluster --zone us-central1-f -q
