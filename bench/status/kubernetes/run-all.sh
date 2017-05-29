2fd282d5044d01a3698a96e4ff4382c928032d9d
vincentserpoul <vincent@serpoul.com>
2017-05-30 00:12:12 +0800

    downsize gcpmysql connections and hide cockroachdb

--- bench/status/kubernetes/run-all.sh
+++ bench/status/kubernetes/run-all.sh
@@ -3,19 +3,20 @@
 export SCH=$1;
 
 ./infra/server/kubernetes/create.sh
+helm init
 
 # Number of connections is based on n1-standard-1 (1 vCPU, 3.75 GB memory) Machine type on gcloud
 
 echo "[" > ./bench/status/kubernetes/$SCH/results.log
 # until we find out what's wrong with cockroachdb'
-# ./infra/databases/kubernetes/cockroachdb/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh cockroachdb cockroachdb $SCH 1000 3;
+# ./infra/databases/kubernetes/cockroachdb/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh cockroachdb cockroachdb $SCH 1000 1;
 ./infra/databases/kubernetes/mssql/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh mssql mssql $SCH 1000 10;
 ./infra/databases/kubernetes/mysql/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh mysql mysql $SCH 1000 100;
 ./infra/databases/kubernetes/mariadb/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh mariadb mariadb $SCH 1000 100;
 ./infra/databases/kubernetes/percona/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh percona percona $SCH 1000 100;
 ./infra/databases/kubernetes/oracle/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh oracle oracle $SCH 1000 1;
 ./infra/databases/kubernetes/postgres/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh postgres postgres $SCH 1000 100;
-./infra/databases/kubernetes/gcpmysql/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh gcpmysql gcpmysql $SCH 1000 1000;
+./infra/databases/kubernetes/gcpmysql/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh gcpmysql gcpmysql $SCH 1000 100;
 ./infra/databases/kubernetes/gcppostgres/launch-solo.sh && ./bench/status/kubernetes/run-bench.sh gcppostgres gcppostgres $SCH 1000 50;
 ./bench/status/kubernetes/run-bench.sh sqlite pws_sqlite $SCH 1000 1;
 
