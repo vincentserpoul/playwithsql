ba110273b1f8552f9ffafede373c67dab7c15cbc
vincentserpoul <vincent@serpoul.com>
2017-06-09 00:32:55 +0800

    10000 loops only

--- bench/status/swarm/run-all.sh
+++ bench/status/swarm/run-all.sh
@@ -6,13 +6,13 @@
 
 echo "[" > ./bench/status/swarm/$SCH/results.log
 # until we find out what's wrong with cockroachdb'
-# ./infra/databases/swarm/cockroachdb/launch-solo.sh && ./bench/status/swarm/run-bench.sh cockroachdb cockroachdb $SCH 1000000 10;
+./infra/databases/swarm/cockroachdb/launch-solo.sh && ./bench/status/swarm/run-bench.sh cockroachdb cockroachdb $SCH 10000 10;
 ./infra/databases/swarm/mssql/launch-solo.sh && ./bench/status/swarm/run-bench.sh mssql mssql $SCH 10000 10;
 ./infra/databases/swarm/mysql/launch-solo.sh && ./bench/status/swarm/run-bench.sh mysql mysql $SCH 10000 10;
 ./infra/databases/swarm/mariadb/launch-solo.sh && ./bench/status/swarm/run-bench.sh mariadb mariadb $SCH 10000 10;
 ./infra/databases/swarm/percona/launch-solo.sh && ./bench/status/swarm/run-bench.sh percona percona $SCH 10000 10;
 ./infra/databases/swarm/postgres/launch-solo.sh && ./bench/status/swarm/run-bench.sh postgres postgres $SCH 10000 10;
-./bench/status/swarm/run-bench.sh sqlite pws_sqlite $SCH 10000 1;
+./bench/status/swarm/run-bench.sh sqlite pws_sqlite $SCH 100000 1;
 
 #  remove last comma
 sed -i '$s/,$//' ./bench/status/swarm/$SCH/results.log;
